9dd514cfd4dfa8cee81bc998b077bef7d98bf987
tsunglung <tsunglung@github.com>
2022-04-06 21:15:36 +0800

    Refactor sensor entity with description

--- custom_components/taipower_bimonthly_cost/const.py
+++ custom_components/taipower_bimonthly_cost/const.py
@@ -1,4 +1,11 @@
 """Constants for TaiPower Bimonthly Energy Cost Integration."""
+from dataclasses import dataclass
+
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntityDescription,
+    SensorStateClass,
+)
 
 CONFIG_FLOW_VERSION = 1
 
@@ -13,3 +20,28 @@ UNIT_KWH_COST = "TWD/kWh"
 UNIT_TWD = "TWD"
 CONF_BIMONTHLY_ENERGY = "bimonthly_energy"
 CONF_METER_START_DAY = "meter_start_day"
+
+
+@dataclass
+class TaiPowerCostSensorDescription(
+    SensorEntityDescription
+):
+    """Class to describe an TaiPower Energy Cost sensor."""
+
+
+COST_SENSORS: tuple[TaiPowerCostSensorDescription, ...] = (
+    TaiPowerCostSensorDescription(
+        key="kwh_cost",
+        name="Price Per kWh",
+        native_unit_of_measurement=UNIT_KWH_COST,
+        device_class=SensorDeviceClass.MONETARY,
+        state_class=SensorStateClass.MEASUREMENT,
+    ),
+    TaiPowerCostSensorDescription(
+        key="power_cost",
+        name="Power Cost",
+        native_unit_of_measurement=UNIT_TWD,
+        device_class=SensorDeviceClass.MONETARY,
+        state_class=SensorStateClass.MEASUREMENT,
+    )
+)
